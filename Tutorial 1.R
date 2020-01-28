aa057fb60d416faeaf456987819d28ad0b139b01
Tomas Kaziukonis <tk852@its.york.ac.uk>
2020-01-28 09:55:21 +0000

    learnt how to use tidyverse to summarise

--- Tutorial 1.R
+++ Tutorial 1.R
@@ -1,6 +1,25 @@
-library(ggplot2)
+library(tidyverse)
+
 #data input
 response <- read.table("response.txt", skip = 6, header = TRUE)
+
+#tidyverse function allows you to summarise all data quickly and easily e.g. not needing $, can use group by etc.
+response %>% #
+  group_by(genotype) %>% #does each command below for each group created
+  summarise(m = mean(sens),
+            sd = sd(sens),
+            n = length(sens),
+            m_gsh = mean(GSH),
+            se = sd/sqrt(n))
+
+#allows you to filter out rows
+response %>% 
+  filter(genotype == 'A2')
+
+#select certain columns
+response %>% 
+  select(GSH)
+
 str(response)
 ggplot(data = response,
        aes(x = GSH, y = sens, col = genotype)) +
